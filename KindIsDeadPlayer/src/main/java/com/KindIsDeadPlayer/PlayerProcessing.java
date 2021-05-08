8f3c69a83754820ed30fcd63401c97280560eb2e
Jaya Emekar <37955495+jayaemekar@users.noreply.github.com>
2021-05-08 17:06:08 +0530

    Update PlayerProcessing.java

--- KindIsDeadPlayer/src/main/java/com/KindIsDeadPlayer/PlayerProcessing.java
+++ KindIsDeadPlayer/src/main/java/com/KindIsDeadPlayer/PlayerProcessing.java
@@ -1,196 +1,123 @@
 package com.KindIsDeadPlayer;
 
+import static java.nio.file.StandardOpenOption.WRITE;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class PlayerProcessing {
+public class Utility {
 
-	/**
-	 * Populate the map randomly. Purpose: Populate each country with 4 tokens
-	 * which includes homeland with 4 tokens of their color. Intialited by:
-	 * Server Data : Country and their followers Message: 02: <Country>, <Blue
-	 * followers>, <Red followers> , < Yellow followers> E.g.
-	 * 02:ES,2,2,0;DE,1,1,2;...
-	 * 
-	 * @param messageNumber
-	 * @param messageDetailsList
-	 */
-	public static void populateMapRandomly(String messageNumber, List<String> messageDetailsList) {
-		System.out.println("Country		Blue	Red		Yellow");
-		System.out.println(messageDetailsList.get(0) + "		" + messageDetailsList.get(1) + "	"
-				+ messageDetailsList.get(2) + "		" + messageDetailsList.get(3) + "");
-		System.out.println(messageDetailsList.get(4) + "		" + messageDetailsList.get(5) + "	"
-				+ messageDetailsList.get(6) + "		" + messageDetailsList.get(7) + "");
-		System.out.println(messageDetailsList.get(8) + "		" + messageDetailsList.get(9) + "	"
-				+ messageDetailsList.get(10) + "		" + messageDetailsList.get(11) + "");
-		System.out.println(messageDetailsList.get(12) + "		" + messageDetailsList.get(13) + "	"
-				+ messageDetailsList.get(14) + "		" + messageDetailsList.get(15) + "");
-		System.out.println(messageDetailsList.get(16) + "		" + messageDetailsList.get(17) + "	"
-				+ messageDetailsList.get(18) + "		" + messageDetailsList.get(19) + "");
-		System.out.println(messageDetailsList.get(20) + "		" + messageDetailsList.get(21) + "	"
-				+ messageDetailsList.get(22) + "		" + messageDetailsList.get(23) + "");
-		System.out.println(messageDetailsList.get(24) + "		" + messageDetailsList.get(25) + "	"
-				+ messageDetailsList.get(26) + "		" + messageDetailsList.get(27) + "");
-		System.out.println(messageDetailsList.get(28) + "		" + messageDetailsList.get(29) + "	"
-				+ messageDetailsList.get(30) + "		" + messageDetailsList.get(31) + "");
+	private String PlayerName;
+	private String writefilepath;
+	private String readfilepath;
+	private static volatile Utility Utility = null;
+
+	Utility() {
+		// private constructor
 	}
 
-	/**
-	 * Distribute two random followers to the players Purpose: Give each player
-	 * 2 random followers for their starting court Initiated by: Server Data:
-	 * Followers Message: 03: <Player Number>, < Blue Follower > < Red Follower
-	 * > <Yellow Followers> Eg: 03:P1,1,0,1
-	 * 
-	 * @param messageNumber
-	 * @param messageDetailsList
-	 */
-	public static void distributeRandomFollowers(String messageNumber, List<String> messageDetailsList) {
-		System.out.println("Player " + messageDetailsList.get(0) + " has " + messageDetailsList.get(1) + " Blue "
-				+ messageDetailsList.get(2) + " Red " + messageDetailsList.get(3) + " Yellow Followers ");
-		System.out.println("Player " + messageDetailsList.get(4) + " has " + messageDetailsList.get(5) + " Blue "
-				+ messageDetailsList.get(6) + " Red " + messageDetailsList.get(7) + " Yellow Followers ");
-		System.out.println("Player " + messageDetailsList.get(8) + " has " + messageDetailsList.get(9) + " Blue "
-				+ messageDetailsList.get(10) + " Red " + messageDetailsList.get(11) + " Yellow Followers ");
-
-	}
-
-	/**
-	 * Randomly number your 8 regions (for power struggle resolution order)
-	 * Purpose: Assign the country randomly for each number Initiated by: Server
-	 * Data: Country Cards Message: 04: < Country 1 > , <Country 2 >, <Country 3
-	 * > …… <Country 8> Eg: 04: MO,NO...ES
-	 * 
-	 * @param messageNumber
-	 * @param messageDetailsList
-	 */
-	public static void randomlyNameRegion(String messageNumber, List<String> messageDetailsList) {
-		System.out.println("Country  	Number");
-		System.out.println(messageDetailsList.get(0) + "  		" + 1);
-		System.out.println(messageDetailsList.get(1) + "  		" + 2);
-		System.out.println(messageDetailsList.get(2) + "  		" + 3);
-		System.out.println(messageDetailsList.get(3) + "  		" + 4);
-		System.out.println(messageDetailsList.get(4) + "  		" + 5);
-		System.out.println(messageDetailsList.get(5) + " 	 	" + 6);
-		System.out.println(messageDetailsList.get(6) + " 	 	" + 7);
-		System.out.println(messageDetailsList.get(7) + " 	 	" + 8);
-
-	}
-
-	public static void playerTurn(String messageNumber, List<String> messageDetailsList) {
-
-		System.out.println("Player " + messageDetailsList.get(0) + " is selected.");
-		if (Utility.getInstance().GetPlayerName().equals(messageDetailsList.get(0))) {
-
-			System.out.println("Which card you wish to play ? ");
-			System.out.println("For card choose from below: ");
-			System.out.println("Supporter card : 1 ");
-			System.out.println("Assemble card : 2 ");
-			System.out.println("Manoeuvre card : 3 ");
-			System.out.println("OutManoeuvre card : 4 ");
-			System.out.println("Neigotiate card : 5 ");
-			System.out.println("Pass the turn  card : 6");
-
-			String cardNum = System.console().readLine();
-
-			if ("1".equals(cardNum))
-				supporterCard();
-			else if ("2".equals(cardNum))
-				assembleCard();
-			else if ("3".equals(cardNum))
-				manoeuvre();
-			else if ("4".equals(cardNum))
-				outManoeuvre();
-			else if ("5".equals(cardNum))
-				negotiate();
-			else if ("6".equals(cardNum))
-				passTheTurn();
+	// singleton object creation
+	public static Utility getInstance() {
+		if (Utility == null) {
+			synchronized (Utility.class) {
+				if (Utility == null) {
+					Utility = new Utility();
+				}
+			}
 		}
+		return Utility;
+	}
+
+	public void SetPlayerName(String name) {
+		this.PlayerName = name;
+	}
+
+	public String GetPlayerName() {
+		return this.PlayerName;
+	}
+
+	public String getFileWritePath() {
+		return writefilepath;
+	}
+
+	public void setFileWritePath(String fileWritePath) {
+		this.writefilepath = fileWritePath;
+	}
+
+
+	public static void readFile(String filename, boolean canBreak) throws InterruptedException, IOException {
+		String line;
+		try {
+			LineNumberReader lnr =new LineNumberReader(new FileReader(filename));
+			while (!canBreak) {
+				line = lnr.readLine();
+				if (line == null || line.isEmpty()) {				
+					Thread.sleep(2400);
+					continue;
+				}
+				canBreak = parseMessage(line);
+				if (canBreak)
+					break;
+			}
+			lnr.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	public static Boolean parseMessage(String message) throws InterruptedException {
+
+		String[] messageArray = message.split(":");
+		String[] messageDetails = messageArray[1].split(",");
+		String messageNumber = messageArray[0];
+		List<String> messageDetailsList = new ArrayList<>(Arrays.asList(messageDetails));
+
+		if ("02".equals(messageNumber))
+			PlayerProcessing.populateMapRandomly(messageNumber, messageDetailsList);
+		else if ("03".equals(messageNumber))
+			PlayerProcessing.distributeRandomFollowers(messageNumber, messageDetailsList);
+		else if ("04".equals(messageNumber))
+			PlayerProcessing.randomlyNameRegion(messageNumber, messageDetailsList);
+		else if ("06".equals(messageNumber))
+			PlayerProcessing.playerTurn(messageNumber, messageDetailsList);
+		else if ("16".equals(messageNumber))
+			PlayerProcessing.winnerplayer(messageNumber, messageDetailsList);
+		else
+			return true;
+
+		return false;
 
 	}
 
-	
-	private static void supporterCard() {
-		System.out.println("Choose Scotish(S) Welish(W) English(E) Support Card");
-		String choice = System.console().readLine();
-		System.out.println("Enter Region to support    " + "Moray : MO\r\n" + "Strathclyde : ST\r\n"
-				+ "Lancaster: LA\r\n" + "Northumbria: NO\r\n" + "Warwick: WA\r\n" + "Gwynedd: GW\r\n" + "Essex: ES\r\n"
-				+ "Devon: DE\r\n" + "France: FR\r\n" + "");
-		String region = System.console().readLine();
-		String message = "07:" + Utility.getInstance().GetPlayerName() + "," + choice + "," + region;
-		Utility.writeFile(Utility.getInstance().getFileWritePath(), message);
-		drawTheSupportor();
+	public static void writeFile(String filePath, String message) {
+		try {
+			message= message+"\n";
+			Path path = Paths.get(filePath);
+			OutputStream outputStream = Files.newOutputStream(path, WRITE);
+			outputStream.write(message.getBytes());
+			outputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	private static void assembleCard() {
-		System.out.println("Enter First Region to support    " + "Moray : MO\r\n" + "Strathclyde : ST\r\n"
-				+ "Lancaster: LA\r\n" + "Northumbria: NO\r\n" + "Warwick: WA\r\n" + "Gwynedd: GW\r\n" + "Essex: ES\r\n"
-				+ "Devon: DE\r\n" + "France: FR\r\n" + "");
-		String region1 = System.console().readLine();
-		System.out.println("Enter the follower first color to add: [R/B/Y]");
-		String color1 = System.console().readLine();
-		System.out.println("Enter Second Region to support    " + "Moray : MO\r\n" + "Strathclyde : ST\r\n"
-				+ "Lancaster: LA\r\n" + "Northumbria: NO\r\n" + "Warwick: WA\r\n" + "Gwynedd: GW\r\n" + "Essex: ES\r\n"
-				+ "Devon: DE\r\n" + "France: FR\r\n" + "");
-		String region2 = System.console().readLine();
-		System.out.println("Enter the follower second color to add: [R/B/Y]");
-		String color2 = System.console().readLine();
-		System.out.println("Enter Third Region to support    " + "Moray : MO\r\n" + "Strathclyde : ST\r\n"
-				+ "Lancaster: LA\r\n" + "Northumbria: NO\r\n" + "Warwick: WA\r\n" + "Gwynedd: GW\r\n" + "Essex: ES\r\n"
-				+ "Devon: DE\r\n" + "France: FR\r\n" + "");
-		String region3 = System.console().readLine();
-		System.out.println("Enter the follower third color to add [R/B/Y]: ");
-		String color3 = System.console().readLine();
-		String message = "08:" + region1 + "," + color1 + "," + region2 + "," + color2 + "," + region3 + "," + color3;
-		Utility.writeFile(Utility.getInstance().getFileWritePath(), message);
-		drawTheSupportor();
+	public String getReadfilepath() {
+		return readfilepath;
 	}
 
-	private static void manoeuvre() {
-		System.out.println("Enter the Region1 seperated by space with color 1 to swap");
-		String part1 = System.console().readLine();
-		String[] data1 = part1.split(" ");
-		System.out.println("Enter the Region2 seperated by space with color 2 to swap");
-		String part2 = System.console().readLine();
-		String[] data2 = part2.split(" ");
-		String message = "09:" + data1[0] + "," + data1[1] + "," + data2[0] + "," + data2[1];
-		Utility.writeFile(Utility.getInstance().getFileWritePath(), message);
-		drawTheSupportor();
-	}
-
-	private static void outManoeuvre() {
-		System.out.println("Enter the Region1 seperated by space with 2 color to swap");
-		String part1 = System.console().readLine(); 
-		String[] data1 = part1.split(" ");
-		System.out.println("Enter the Region2 seperated by space with color 1 to swap");
-		String part2 = System.console().readLine();
-		String[] data2 = part2.split(" ");
-		String message = "10:" + data1[0] + "," + data1[1] + "," + data2[0] + "," + data2[1];
-		Utility.writeFile(Utility.getInstance().getFileWritePath(), message);
-		drawTheSupportor();
-	}
-	
-	private static void negotiate() {
-		System.out.println("Negotiate: Enter country to swap from");
-		String country1 = System.console().readLine();
-		System.out.println("Enter country to swap with");
-		String country2 = System.console().readLine();
-		String message = "11:" + country1 + "," + country2;
-		Utility.writeFile(Utility.getInstance().getFileWritePath(), message);
-		drawTheSupportor();
-	}
-	
-	private static void passTheTurn() {
-		System.out.println(" Trun passed to other player. ");
-		String message = "15:Pass";
-		Utility.writeFile(Utility.getInstance().getFileWritePath(), message);
-	}
-	
-	private static void drawTheSupportor(){
-		System.out.println("Please Enter the follower : (B/Y/R)");
-		String part1 = System.console().readLine(); 
-		System.out.println("Please Enter the country name :");
-		String part2 = System.console().readLine(); 
-		String message = "12" + part1 + "," + part2;
-		Utility.writeFile(Utility.getInstance().getFileWritePath(), message);
+	public void setReadfilepath(String readfilepath) {
+		this.readfilepath = readfilepath;
 	}
 
 }
